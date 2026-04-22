f4f9db43bae8813071a8b59a46c17ec265e7147e
Deepu916 <deepuenas999@gmail.com>
2026-04-22 17:42:22 +0530

    Add files via upload

--- models/sale_order.py
+++ models/sale_order.py
@@ -1,42 +1,41 @@
 # -*- coding: utf-8 -*-
 """Sale Order"""
-from odoo import  models
+from odoo import  fields,models
 
 
 class SaleOrder(models.Model):
     """Sale Order Model"""
     _inherit = 'sale.order'
 
+
+    project_id = fields.Many2one('project.project',string="Project")
+    created = fields.Boolean(string="Create Date")
     def action_create_so_project(self):
         """Create project from sale order"""
         milestones = {}
         # Creating the project
-        project = self.env['project.project'].create({
+        self.project_id = self.env['project.project'].create({
             'name': self.name,
             'partner_id': self.partner_id.id,
             'company_id':self.company_id.id,
             'user_id': self.env.user.id,
         })
-        parent_task = []
+        self.created = True
+        names = ['New','In Progress','Done','Cancelled']
+        demo = self.env['project.task.type'].search([('name','in',names),('user_id','!=',self.env.user.id)])
+        demo.write({'project_ids':[fields.Command.link(self.project_id.id)]})
+        self.project_count = len(self.project_id)
         for line in self.order_line:
-            if line.milestone not in milestones:
-                milestones[line.milestone] = [line.product_template_id.name]
-                parent = (self.env['project.task'].create({
-                    'name': "Milestone " + str(line.milestone),
-                    'project_id': project.id,
-                }))
-                self.env['project.task'].create({
-                    'name': "Milestone " + str(line.milestone) + ' - '+ line.product_template_id.name,
-                    'project_id': project.id,
-                    'parent_id':parent.id
+            milestone_key = line.milestone
+            if milestone_key not in milestones:
+                parent = self.env['project.task'].create({
+                    'name': "Milestone " + str(milestone_key),
+                    'project_id': self.project_id.id,
                 })
-                parent_task.append(parent)
-            else:
-                milestones[line.milestone].append(line.product_template_id.name)
-                for p in parent_task:
-                    if p.name == "Milestone " + str(line.milestone):
-                        self.env['project.task'].create({
-                            "name": "Milestone " + str(line.milestone) + ' - '+ line.product_template_id.name,
-                            "project_id": project.id,
-                            "parent_id": p.id,
-                        })
+                milestones[milestone_key] = parent
+            self.env['project.task'].create({
+                'name': "Milestone " + str(milestone_key)+ "-" + line.product_template_id.name,
+                'project_id': self.project_id.id,
+                'parent_id': milestones[milestone_key].id,
+            })
+        
