4c461476c9d7599cc71731a7ddd68431852aff08
Deepu916 <deepuenas999@gmail.com>
2026-04-24 17:44:23 +0530

    Add files via upload

--- models/sale_order.py
+++ models/sale_order.py
@@ -10,6 +10,7 @@ class SaleOrder(models.Model):
 
     project_id = fields.Many2one('project.project',string="Project")
     created = fields.Boolean(string="Create Date")
+    parents = {}
     def action_create_so_project(self):
         """Create project from sale order"""
         milestones = {}
@@ -25,7 +26,6 @@ class SaleOrder(models.Model):
         names = ['New','In Progress','Done','Cancelled']
         demo = self.env['project.task.type'].search([('name','in',names),('user_id','!=',self.env.user.id)])
         demo.write({'project_ids':[fields.Command.link(self.project_id.id)]})
-        self.project_count = len(self.project_id)
         for line in self.order_line:
             milestone_key = line.milestone
             if milestone_key not in milestones:
@@ -37,6 +37,7 @@ class SaleOrder(models.Model):
 
                 })
                 milestones[milestone_key] = parent
+                self.parents[milestone_key] = parent
             self.env['project.task'].create({
                 'name': "Milestone " + str(milestone_key)+ "-" + line.product_template_id.name,
                 'project_id': self.project_id.id,
@@ -44,8 +45,46 @@ class SaleOrder(models.Model):
                 'sale_order_id': self.id,
                 'sale_line_id': line.id,
             })
+
     def action_update_so_project(self):
         """Update project from sale order"""
+        print("Parents",self.parents)
+        Task = self.env['project.task']
+        existing_tasks = self.project_id.task_ids
+        task_mapping ={}
+        for task in existing_tasks:
+            if task.sale_line_id:
+                task_mapping[task.sale_line_id.id] = task
         for line in self.order_line:
-            if line.id not in self.project_id.sale_line_id:
-                
+            milestone_key = line.milestone
+            if milestone_key not in self.parents:
+                parent = Task.create({
+                    'name': "Milestone " + str(milestone_key),
+                    'project_id': self.project_id.id,
+                    'sale_order_id': self.id,
+                    'sale_line_id': line.id,
+                })
+
+                self.parents[milestone_key] = parent
+            if self.parents[milestone_key] not in self.env['project.task'].search([('project_id','=',self.project_id.id)]):
+                parent = self.env['project.task'].create({
+                    'name': "Milestone " + str(milestone_key),
+                    'project_id': self.project_id.id,
+                    'sale_order_id': self.id,
+                    'sale_line_id': line.id,
+                })
+                self.parents[milestone_key] = parent
+            existing_parent_id = self.parents[milestone_key]
+            existing_task = task_mapping.get(line.id)
+            if not existing_task and line.product_uom_qty > 0:
+                Task.create({
+                    'name': "Milestone " + str(milestone_key) + "-" + line.product_template_id.name,
+                    'project_id': self.project_id.id,
+                    'parent_id': existing_parent_id.id,
+                    'sale_order_id': self.id,
+                    'sale_line_id': line.id,
+                })
+
+        task_to_delete = existing_tasks.filtered(lambda task: task.sale_line_id.product_uom_qty == 0)
+        task_to_delete.unlink()
+        print(self.parents)
